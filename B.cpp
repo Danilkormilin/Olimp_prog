a24b43d589e005373efaba9ec4f7903f1f397ba9
Danilkormilin <64023679+Danilkormilin@users.noreply.github.com>
2020-04-20 19:53:01 +0500

    Create B.cpp

---
+++ B.cpp
@@ -0,0 +1,52 @@
+#include<stdio.h>
+#include<math.h>
+int stack[100];
+char cmd[10];
+int last = 0;
+int main()
+{
+	while (1)
+	{
+		scanf("%s", &cmd);
+		if (cmd[0] == 'p' && cmd[1] == 'o')
+		{
+			if (last != 0)
+			{
+				printf("%d\n", stack[last]);
+				last--;
+			}
+			else printf("error\n");
+
+		}
+		else if (cmd[0] == 'p' && cmd[1] == 'u')
+		{
+			last++;
+			scanf("%d", &stack[last]);
+			printf("ok\n");
+		}
+		else if (cmd[0] == 's' && cmd[1] == 'i')
+		{
+			printf("%d\n", last);
+		}
+		else if (cmd[0] == 'b' && cmd[1] == 'a')
+		{
+			if (last != 0)
+			{
+				printf("%d\n", stack[last]);
+			}
+			else printf("error\n");
+		}
+		else if (cmd[0] == 'c' && cmd[1] == 'l')
+		{
+			last = 0;
+			printf("ok\n");
+		}
+		else
+		{
+			printf("bye");
+			return(0);
+		}
+
+	}
+	return (0);
+}
